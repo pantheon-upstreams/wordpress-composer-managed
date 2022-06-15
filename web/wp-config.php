02f2e40aba544570ebf15cd83394001af8d5b5c0
jspellman814 <john.spellman@pantheon.io>
2022-06-15 14:28:22 -0600

    Pantheon-ify bedrock

--- web/wp-config.php
+++ web/wp-config.php
@@ -4,6 +4,14 @@
  * This file is required in the root directory so WordPress can find it.
  * WP is hardcoded to look in its own directory or one directory up for wp-config.php.
  */
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-require_once dirname(__DIR__) . '/config/application.php';
-require_once ABSPATH . 'wp-settings.php';
+
+/**
+ * Pantheon platform settings. Everything you need should already be set.
+ */
+if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
+} else {
+    require_once dirname(__DIR__) . '/vendor/autoload.php';
+    require_once dirname(__DIR__) . '/config/application.php';
+    require_once ABSPATH . 'wp-settings.php';
+}
